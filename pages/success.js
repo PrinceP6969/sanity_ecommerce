4f87e09a3f0a1820053a4b47eed5bbaefb4c2e10
prince <princepandey6969@gmail.com>
2023-05-29 08:42:46 +0545

    first commit

---
+++ pages/success.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { BsBagCheckFill } from "react-icons/bs";
+
+import { useStateContext } from "../context/StateContext";
+import confetti from "canvas-confetti";
+
+const Success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+    confetti({
+      particleCount: 100,
+      spread: 70,
+      origin: { y: 0.6 },
+    });
+  }, []);
+
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Thank you for your order!</h2>
+        <p className="email-msg">Check your email inbox for the receipt.</p>
+        <p className="description">
+          If you have any questions, please email
+          <a className="email" href="mailto:pandeyprince69696@gmail.com">
+            pandeyprince69696
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" width="300px" className="btn">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
---
+++ sanityecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '6ds4srq8',
+    dataset: 'production'
+  }
+})
